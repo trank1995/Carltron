14bf76a1ae0d91a1a8fbd5b044bfa14916268c2b
Frederik Stensaeth <stensaethf@carleton.edu>
2015-05-28 19:44:46 -0500

    main setup

--- src/carltron/Main.java
+++ src/carltron/Main.java
@@ -1,16 +1,34 @@
 package carltron;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("carltron-menu.fxml"));
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+        FXMLLoader loader = new FXMLLoader(getClass().getReasource
+                ("carltron-menu.fxml"));
+
+        Parent root = (Parent)loader.load();
+        GameManager gameManager = loader.getController();
+        root.setOnKeyPressed(gameManager);
+
         primaryStage.setTitle("CarlTron");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
