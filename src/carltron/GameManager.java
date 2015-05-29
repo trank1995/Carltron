29fb3d24fd35ae1bb96c2d1f36e8f949251f8ca3
Derek Shang <shangd@eduroam-171-174.dyn.carleton.edu>
2015-05-28 20:06:50 -0500

    Implemented LightCycle.java

--- src/carltron/GameManager.java
+++ src/carltron/GameManager.java
@@ -7,7 +7,7 @@ import javafx.geometry.Point2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Lavel;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
