409f1ec1d997227ef1748548024cc24ab233ddbc
Derek Shang <shangd@eduroam-171-174.dyn.carleton.edu>
2015-05-28 19:05:57 -0500

    I don't know

--- src/carltron/LightCycle.java
+++ src/carltron/LightCycle.java
@@ -1,7 +1,15 @@
 package carltron;
 
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+
 /**
  * Created by shangd on 5/27/15.
  */
-public class LightCycle {
+public class LightCycle extends Rectangle {
+    private int velocityX;
+    private int velocityY;
+    private Color color;
+    private Bonus[] bonuses;
+    private int life;
 }
