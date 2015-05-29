3b7d183b790b777bfb1c976d4fc946cb27e3b4b2
trank <trank@cmc304-02.mathcs.carleton.edu>
2015-05-28 19:54:29 -0500

    hail hydra

--- src/carltron/Jump.java
+++ src/carltron/Jump.java
@@ -4,4 +4,14 @@ package carltron;
  * Created by shangd on 5/27/15.
  */
 public class Jump extends Bonus {
+    /**The constructor for the jump class. Takes an integer to represent the amount of turbos available (newAmount) to the LightCycle and sets amount to be this number.
+     **/
+    public Jump(int newAmount) {
+    }
+    /**Consumes a potion of jump and gains the ability to cross a path and leave no path behind while the potion is in effect.**/
+    public void consume(){
+        
+    }
+
+
 }
