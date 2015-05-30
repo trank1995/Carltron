2c70edcf42f1540a801cf33d0826e4082e2dcfe3
Frederik Stensaeth <stensaethf@carleton.edu>
2015-05-30 10:56:26 -0500

    grid path

--- src/carltron/Grid.java
+++ src/carltron/Grid.java
@@ -1,16 +1,28 @@
 package carltron;
 
+import java.util.*;
+import javafx.scene.shape.Rectangle;
+
 /**
  * Created by shangd on 5/27/15.
  */
 public class Grid {
     private int width;
     private int height;
+    private ArrayList<Rectangle> path = new ArrayList<Rectangle>();
 
     public Grid(){
         this.width = 500;
         this.height = 500;
 
+    }
+
+    public void addToGrid(Rectangle new_path) {
+        path.add(new_path);
+    }
+
+    public boolean collisionWithPath(Rectangle bike) {
+        x;
     }
 
     private int getWidth(){
