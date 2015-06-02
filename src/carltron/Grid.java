8622ce8044509bf75853d4e1dd612ea7f8fcf418
Frederik Stensaeth <stensaethf@carleton.edu>
2015-06-01 23:53:46 -0500

    gameManager commented

--- src/carltron/Grid.java
+++ src/carltron/Grid.java
@@ -1,24 +1,29 @@
 package carltron;
-/**
- * Grid.java
- * This class will (1) record the path of lightcycle
- * (2) detect collision
- * June 1, 2015 - version 1
- */
+
 import javafx.scene.shape.Rectangle;
 import java.util.ArrayList;
 
-
+/**
+ * Grid.java
+ * This class will (1) record the path of LightCycles
+ * (2) detect collision between LightCycles and the paths.
+ * June 1, 2015 - version 1
+ */
 public class Grid {
     //an arraylist of path
     private ArrayList<Rectangle> paths = new ArrayList<Rectangle>();
 
+    /**
+     * The constructor for Grid. Takes no parameters and sets up nothing.
+     * */
     public Grid() {
     }
 
     /**
-     * Add the new path into the arraylsit
-     * @param path
+     * addToGrid(Rectangle) adds a rectangle to into the arrayList, which
+     * enables us to update the path the LightCycles leave behind them.
+     *
+     * @params path
      */
     public void addToGrid(Rectangle path) {
         if (path != null) {
@@ -27,7 +32,9 @@ public class Grid {
     }
 
     /**
-     * return the arraylist of path so we can draw
+     * getGridList() returns the arraylist of path so we can draw.
+     * This method is used for testing.
+     *
      * @return ArrayList<Rectangle>
      */
     public ArrayList<Rectangle> getGridList() {
@@ -35,9 +42,10 @@ public class Grid {
     }
 
     /**
-     * detect collision of bike and path
+     * collisionWithPath(LightCycle) detects collision of bike and path
+     *
      * @param bike
-     * @return true if collide
+     * @return true or false
      */
     public boolean collisionWithPath(LightCycle bike) {
         if (bike != null) {
